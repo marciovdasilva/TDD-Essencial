c3aad55ee10570e18ccfcdc1893c1a011e1fdd6f
marciovdasilva <marciov.dasilva@gmail.com>
2022-09-02 21:06:14 -0300

    Primeiro commit do curso de TDD

---
+++ bin/test/CamelCaseConverterTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CamelCaseConverterTest {
+
+	private CamelCaseConverter camelCase;
+	
+	@Before
+	public void setup() {
+		camelCase = new CamelCaseConverter();		
+	}
+	
+	
+	@Test
+	public void deveCriarObjetoCamelCaseConverter() throws Exception {
+		
+	}
+	
+	@Test
+	public void aplicarCamelCaseEmnomeUnico() throws Exception {
+		assertEquals("Lionel", camelCase.converter("lionel"));
+	}
+	
+	@Test
+	public void deveConverterNomeSimplesMisturadoMaiusculoEMinusculo() throws Exception {
+		assertEquals("Lionel", camelCase.converter("lIOneL"));
+		
+	}
+	
+	/*fazer 
+	 para nome composto dois, tres ou quatro nomes
+	 
+	 */
+	
+	
+	
+}
