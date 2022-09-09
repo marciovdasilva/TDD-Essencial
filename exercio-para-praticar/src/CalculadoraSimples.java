9073917f55d3557d50fa9b62f66df8d6d46875f4
marciovdasilva <marciov.dasilva@gmail.com>
2022-09-08 21:36:34 -0300

    Commit do exercicio de treinamento.

---
+++ exercio-para-praticar/src/CalculadoraSimples.java
@@ -0,0 +1,21 @@
+public class CalculadoraSimples {
+	public int adicao(int valorA, int valorB) {
+		return valorA + valorB;
+	}
+
+	public int subtracao(int valorA, int valorB) {
+		return valorA - valorB;
+	}
+
+	public int multiplicacao(int valorA, int valorB) {
+		return valorA * valorB;
+	}
+
+	public int divisao(int dividendo, int divisor) {
+		return dividendo / divisor;			
+
+	}
+	
+	
+	
+}
---
+++ exercio-para-praticar/test/CalculadoraTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CalculadoraTest {
+
+	private CalculadoraSimples calculadora;
+	
+	@Before
+	public void setup() {
+		calculadora = new CalculadoraSimples();		
+	}
+	
+	@Test
+	public void adicaoSimples() throws Exception {
+        int valorA = 15;
+        int valorB = 25;
+        int total = valorA + valorB;
+
+        int adicao = calculadora.adicao(valorA, valorB);
+
+        assertEquals(total, adicao);
+	}
+	@Test
+	public void subtracaoSimplesMaiorNumeroPrimeiro() throws Exception {
+		int valorA = 100;
+		int valorB = 50;
+		int total = valorA - valorB;
+		
+		int subtracao = calculadora.subtracao(valorA, valorB);
+		
+		assertEquals(total, subtracao);
+	}
+	@Test
+	public void subtracaoSimplesMenorNumeroPrimeiro() throws Exception {
+		int valorA = 50;
+		int valorB = 100;
+		int total = valorA - valorB;
+		
+		int subtracao = calculadora.subtracao(valorA, valorB);
+		
+		assertEquals(total, subtracao);
+	}
+	@Test
+	public void multiplicacaoSimples() throws Exception {
+		int valorA = 2;
+		int valorB = 15;
+		int total = valorA * valorB;
+		
+		int multiplicacao = calculadora.multiplicacao(valorA, valorB);
+		
+		assertEquals(total, multiplicacao);
+	}
+	@Test
+	public void divisaoSimples() throws Exception {
+		int dividendo = 10;
+		int divisor = 0;
+		int total = dividendo / divisor;
+		
+		int divisao = calculadora.divisao(dividendo, divisor);
+		
+		assertEquals(0, divisao);
+	}
+}
+
